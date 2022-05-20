9886e4245fc58d7076761679025bfe2f0cb4a3d8
Dean James Morgan <deanmorgan@mediablox.co.uk>
2022-05-20 22:39:03 +0100

    Fixes linting issues after upgrading lint version

--- test/run.js
+++ test/run.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var sassTrue = require('sass-true');
-var glob = require('glob');
+const sassTrue = require('sass-true');
+const glob = require('glob');
+const path = require('path');
 
 // Run tests
 glob.sync('test/_*.scss').forEach(function (file) {
-  sassTrue.runSass({ file: file }, describe, it);
+  console.log("File: " + file);
+  sassTrue.runSass({describe, it},file);
 });
